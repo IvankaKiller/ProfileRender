502e7731034db861055a0a66e9d34b5224123d9c
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-08-18 12:19:32 +0300

    test

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const URL = require("url");
+const VM = require("vm");
+
+const SuccessStatus = 200;
+const ErrorStatus = 400;
 
 module.exports = (Request, Result) => {
 	try{
 		const QueryObject = URL.parse(Request.url, true).query || {};
 		const Type = QueryObject.type || "notype";
 
+		Result.setHeader("Content-Type", "text/plain; charset=utf-8");
+
 		if(Type === "notype"){
 			Result.end("Не указан тип");
 		}
 
 		if(Type === "js"){
-			Result.end("привет)))");
+			const Base64Code = QueryObject.code;
+
+			if(!Base64Code){ throw new Error("Не указан код!"); }
+
+			const Code = Buffer.from(Base64Code, "base64").toString("utf8");
+
+			const Sandbox = {
+				console,
+				Result: null,
+				Request,
+				process: { env: {} }
+			};
+
+			VM.createContext(Sandbox);
+			VM.runInContext(Code, Sandbox);
+
+			Result.statusCode = SuccessStatus;
+			Result.end(String(Sandbox.Result || "Код был успешно вызван, используйте \"Result = ...\" в вашем коде что-бы вывести результат!"))
+			return
 		}
 
-		Result.statusCode = 400;
-		Result.end("Неизвестный \"type\"!")
+		throw new Error("Неизвестный \"type\"!");
 	}catch(e){
-		Result.statusCode = 500;
+		Result.statusCode = ErrorStatus;
 		Result.end("Ошибка серверного скрипта: " + e.message)
 	}
 };
